9acea24e6a5364c7121476e5c707e03182f71edc
Elecia <elecia@logicalelegance.cocm>
2019-09-12 15:07:21 -0700

    who took all the symbols away in the python code? How? Whhhhhyyyyy? Anyway, this is the code that was one the cat.

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-# Purrty cat code
-# This runs on a Trinket-M0
-
-# Hook conductive tape to D3
-# Hook motor FET to D1
-# Power everything 3-6V of current with more than 15mA
-# If it is working, the dotstar should go off leaving only a green LED on.
+# Trinket IO demo
+# Welcome to CircuitPython 3.1.1 :)
 
 import board
 from digitalio import DigitalInOut, Direction, Pull
@@ -28,28 +23,28 @@ motorGnd = DigitalInOut(board.D0)
 motorGnd.direction = Direction.OUTPUT
 motorGnd.value = 0
 
-#print(STARTING PURRTY CAT)
+#print("STARTING PURRTY CAT")
 
 PURR_INCREMENT = 25
 PURR_DECREMENT = 200
-PURR_MINIMUM   = 175256
-PURR_MAXIMUM   = 185256  # must have space for 16 bit value
+PURR_MINIMUM   = 175*256
+PURR_MAXIMUM   = 185*256  # must have space for 16 bit value
 purrValue = 0
-while True
-  if touch.value
-    if purrValue  PURR_MINIMUM
+while True:
+  if touch.value:
+    if purrValue < PURR_MINIMUM:
       purrValue = PURR_MINIMUM
     purrValue += PURR_INCREMENT
-  else 
+  else: 
     purrValue -= PURR_DECREMENT
 
-  if purrValue  0 purrValue = 0
+  if purrValue < 0: purrValue = 0
 
-  if purrValue  PURR_MAXIMUM purrValue = PURR_MAXIMUM    
+  if purrValue > PURR_MAXIMUM: purrValue = PURR_MAXIMUM    
 
   # set analog output to 0-3.3V (0-65535 in increments)
   aout.value = purrValue
-#  print(Touch {} , Purr {}.format(touch.raw_value, purrValue256))
+#  print("Touch {} , Purr {}".format(touch.raw_value, purrValue/256))
   time.sleep(0.1) # make bigger to slow down
 
-#print(EXITING PURRTY CAT)
+#print("EXITING PURRTY CAT")
